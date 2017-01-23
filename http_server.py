8000e9f10557b0f015b527bd2bca6f5054a7e5c0
Jerry Bearer <jbearer@gmail.com>
2017-01-22 16:28:54 -0800

    finsihed class work

--- http_server.py
+++ http_server.py
@@ -16,14 +16,28 @@ def server(log_buffer=sys.stderr):
             conn, addr = sock.accept()  # blocking
             try:
                 print('connection - {0}:{1}'.format(*addr), file=log_buffer)
+                request = ""
                 while True:
                     data = conn.recv(1024)
-                    # print('data:', data)
-                    if len(data) < 1024:
+                    request += data.decode('utf8')
+                    if len(data) < 1024 or not data:
                         break
-                    print('sending response', file=log_buffer)
-                    response = response_ok()
-                    conn.sendall(response)
+                
+                try:   
+                    uri = parse_request(request)
+                except NotImplementedError:
+                    response = response_method_not_allowed()
+                else:
+                    try:
+                        content, mime_type, = resolve_uri(url)
+                    except NameError:
+                        response = response_not_found()
+                    else:
+                        response = response_ok(content, mime_type)
+
+                print('sending response', file=log_buffer)
+                # response = response_ok()
+                conn.sendall(response)
                     # print('received "{0}"'.format(data), file=log_buffer)
                     # if data:
                     #     print('sending data back to client', file=log_buffer)
@@ -47,6 +61,25 @@ def response_ok():
     resp.append(b"Content-Type: text/plain")
     resp.append(b"")
     resp.append(b"this is a pretty minimal respose")
+    return b"\r\n".join(resp)
+
+def parse_request(request):
+    first_line = request.split("\r\n", 1)[0]
+    method, uri, protocol = first_line.split()
+    print("first_line:", first_line)
+    print("method:", method)
+    print("uri:", uri)
+    print("protocol:", protocol)
+    if method != "GET":
+        raise NotImplementedError("We only accept GET")
+    print("request is ok", file=sys.stderr)
+    return uri
+
+def response_method_not_allowed():
+    """returns a 405 Method Not Allowed response"""
+    resp = []
+    resp.append(b"HTTP/1.1 405 Method Not Allowed")
+    resp.append(b"")
     return b"\r\n".join(resp)
 
 
